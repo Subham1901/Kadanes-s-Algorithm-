9a2756d53cab7b5702132d665e4c2ea7aa138827
SUBHAM PRASAD PANDA <79370554+Subham1901@users.noreply.github.com>
2021-06-16 22:27:31 +0530

    Add files via upload

---
+++ Kadane's_Algo.cpp
@@ -0,0 +1,46 @@
+//Author Subham
+//kadane's algorithm..... Maximum sum of a subarray
+#include <bits/stdc++.h>
+
+using namespace std;
+int MaxSum(int a[], int n)
+{
+    int current_sum = 0;
+    int maxsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        current_sum = current_sum + a[i];
+        if (current_sum > maxsum)
+        {
+            maxsum = current_sum;
+        }
+        if (current_sum < 0)
+        {
+            current_sum = 0;
+        }
+    }
+    if (maxsum <= 0)
+    {
+        cout << 0;
+    }
+    return maxsum;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 1)
+    {
+        cout << "Invalid Size";
+        return 0;
+    }
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << MaxSum(a, n);
+    return 0;
+}
